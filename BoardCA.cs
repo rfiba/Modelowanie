3e746bcdc9536073a90e5c8c5b2cf4df1ce45cb4
rfiba <rafal.fiba@hotmail.com>
2019-05-12 16:39:42 +0200

    Klasa BoardCA dodana

---
+++ BoardCA.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelowanie_GUI
+{
+    class BoardCA
+    {
+        AdvancedBoard[] boards;
+        int sizeM;
+        int sizeN;
+
+        public BoardCA(int sizeM, int sizeN)
+        {
+            boards = new AdvancedBoard[2];
+            this.sizeM = sizeM;
+            this.sizeN = sizeN;
+            boards[0] = new AdvancedBoard(sizeM, sizeN);
+            boards[1] = new AdvancedBoard(sizeM, sizeN);
+        }
+    }
+}
