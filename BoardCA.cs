dc7895a6f7a7712b22a184c417bb89fac4f68449
rfiba <rafal.fiba@hotmail.com>
2019-05-12 19:18:15 +0200

    computeStepAbsorbingBoundaryCondition - rozpoczete

--- BoardCA.cs
+++ BoardCA.cs
@@ -50,5 +50,47 @@ namespace Modelowanie_GUI
                 }
             }
         }
+
+        public void computeStepAbsorbingBoundaryCondition(int numberOfBoard, bool Moore = false)
+        {
+            //Nie będzie działać dla rogów
+            for (int i = 1; i < boards[numberOfBoard].SizeM-1; i++)
+            {
+                for (int j = 1; j < boards[numberOfBoard].SizeN-1; j++)
+                {
+                    int[] arr = new int[8];
+
+                    if(i -1 > 0)
+                        arr[0] = boards[numberOfBoard].getValue(BoardGameOfLife.mod(i - 1, boards[numberOfBoard].SizeM), j);
+                    if (i + 1 < boards[numberOfBoard].SizeM - 1)
+                        arr[1] = boards[numberOfBoard].getValue(BoardGameOfLife.mod(i + 1, boards[numberOfBoard].SizeM), j);
+                    if (j + 1 < boards[numberOfBoard].SizeN - 1)
+                        arr[2] = boards[numberOfBoard].getValue(i, BoardGameOfLife.mod(j + 1, boards[numberOfBoard].SizeN));
+                    if (j - 1 > 0)
+                        arr[3] = boards[numberOfBoard].getValue(i, BoardGameOfLife.mod(j - 1, boards[numberOfBoard].SizeN));
+                    if (Moore)
+                    {
+                        if (i - 1 > 0 && j - 1 > 0)
+                            arr[4] = boards[numberOfBoard].getValue(BoardGameOfLife.mod(i - 1, boards[numberOfBoard].SizeM),
+                            BoardGameOfLife.mod(j - 1, boards[numberOfBoard].SizeN));
+                        if (i + 1 < boards[numberOfBoard].SizeM - 1  && j - 1 > 0)
+                            arr[5] = boards[numberOfBoard].getValue(BoardGameOfLife.mod(i + 1, boards[numberOfBoard].SizeM),
+                            BoardGameOfLife.mod(j - 1, boards[numberOfBoard].SizeN));
+                        if (i - 1 > 0 && j + 1 < boards[numberOfBoard].SizeN - 1)
+                            arr[6] = boards[numberOfBoard].getValue(BoardGameOfLife.mod(i - 1, boards[numberOfBoard].SizeM),
+                            BoardGameOfLife.mod(j + 1, boards[numberOfBoard].SizeN));
+                        if (i + 1 < boards[numberOfBoard].SizeM - 1 && j + 1 < boards[numberOfBoard].SizeN - 1)
+                            arr[7] = boards[numberOfBoard].getValue(BoardGameOfLife.mod(i + 1, boards[numberOfBoard].SizeM),
+                            BoardGameOfLife.mod(j + 1, boards[numberOfBoard].SizeN));
+                    }
+                    var groups = arr.GroupBy(v => v);
+                    int maxCount = groups.Max(g => g.Count());
+                    int mode = groups.First(g => g.Count() == maxCount).Key;
+                    boards[(numberOfBoard + 1) % 2].setValue(i, j, mode);
+                }
+            }
+        }
+
+        ch
     }
 }
