1a9a4f61f7c34f92d5556e10e0b136ecf8f5e477
rfiba <rafal.fiba@hotmail.com>
2019-06-04 18:00:57 +0200

    AdvancedCell dodanie kolejnych informacji do ziarna

--- AdvancedCell.cs
+++ AdvancedCell.cs
@@ -12,10 +12,13 @@ namespace Modelowanie_GUI
         public double xCenter { get; set; }
         public double yCenter { get; set; }
         private int energy;
+        bool recrystallizated;
+        private double dislocationDensity;
         public AdvancedCell()
         {
             this.state = 0;
-            this.energy = 01;
+            this.energy = 0;
+            this.recrystallizated = false;
         }
 
         public AdvancedCell(double xCenter, double yCenter) {
@@ -31,6 +34,18 @@ namespace Modelowanie_GUI
         {
             get { return this.energy; }
             set { this.energy = value; }
+        }
+
+        public double DislocationDensity
+        {
+            get { return this.dislocationDensity; }
+            set { this.dislocationDensity = value; }
+        }
+
+        public bool Recrystallizated
+        {
+            get { return this.recrystallizated; }
+            set { this.recrystallizated = value; }
         }
     }
 }
